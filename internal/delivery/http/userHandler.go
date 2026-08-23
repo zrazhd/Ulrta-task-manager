b2624eeb18d1f4f15603725a005d7554dd31c368
Denis <denis.zrazh@gmail.com>
2026-08-23 23:14:35 +0500

    done task handler and started user handler

--- internal/delivery/http/userHandler.go
+++ internal/delivery/http/userHandler.go
@@ -1,6 +1,7 @@
 package httpHandler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/zrazhd/Ulrta-task-manager/internal/usecase"
@@ -14,12 +15,29 @@ func NewUserHandler(service *usecase.UserService) *UserHandler {
 	return &UserHandler{service: service}
 }
 
-func (service *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
+func (handler *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
 	var req struct {
 		Name     string `json:"name"`
 		Email    string `json:"email"`
-		Password string `json:password`
+		Password string `json:"password"`
 	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, err := handler.service.RegisterUser(req.Name, req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusCreated)
+
 }
 
 func (service *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
