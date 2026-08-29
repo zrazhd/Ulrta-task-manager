f4dcb665d3d30a9ca5d48909e8c8bb13b7c66125
Denis <denis.zrazh@gmail.com>
2026-08-29 11:48:45 +0500

    added cache

--- go.mod
+++ go.mod
@@ -9,9 +9,13 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/redis/go-redis/v9 v9.22.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
 )
---
+++ internal/repository/redis/cache.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Cache[T any] struct {
+	client *redis.Client
+	prefix string
+	ttl    time.Duration
+}
+
+func NewCache[T any](client *redis.Client, prefix string, ttl time.Duration) *Cache[T] {
+	return &Cache[T]{client: client, prefix: prefix, ttl: ttl}
+}
+
+func (c *Cache[T]) key(id string) string {
+	return c.prefix + ":" + id
+}
+
+func (c *Cache[T]) Set(ctx context.Context, id string, data *T) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("cant umarshal data adding to cache: %w", err)
+	}
+
+	err = c.client.Set(ctx, c.key(id), jsonData, c.ttl).Err()
+	return err
+}
+
+func (c *Cache[T]) Get(ctx context.Context, id string) (*T, error) {
+	var data T
+
+	jsonData, err := c.client.Get(ctx, c.key(id)).Result()
+
+	if err != nil {
+		if err == redis.Nil {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("no data in cache: %w", err)
+	}
+
+	err = json.Unmarshal([]byte(jsonData), &data)
+	if err != nil {
+		return nil, fmt.Errorf("cant unmarshal cache: %w", err)
+	}
+	return &data, nil
+}
+
+func (c *Cache[T]) Del(ctx context.Context, id string) error {
+	err := c.client.Del(ctx, c.key(id)).Err()
+	return err
+}
